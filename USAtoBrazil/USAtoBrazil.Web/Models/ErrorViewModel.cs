e675e1dd26dbcdf24702e51ddae3ec8180678dc1
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-02-14 16:32:12 +0200

    Add Web Project (FrontEnd)

---
+++ USAtoBrazil/USAtoBrazil.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace USAtoBrazil.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
